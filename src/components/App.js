3898568a61ca84ba7feb41b04350098e7b36c4b4
harvey.zh <hyzhai@ucdavis.edu>
2018-12-26 00:32:17 -0800

    feat: get initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { handleReceiveUsers } from '../actions/users';
+import { handleReceiveQuestions } from '../actions/questions';
 
 class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleReceiveQuestions())
+        this.props.dispatch(handleReceiveUsers())
+    }
+
   render() {
     return (
       <div>
