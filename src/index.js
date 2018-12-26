2fe801a91000a758dd21caa3fc433ffeb2fa4f6d
harvey.zh <hyzhai@ucdavis.edu>
2018-12-26 00:29:54 -0800

    feat: provide store to App

--- src/index.js
+++ src/index.js
@@ -6,5 +6,12 @@ import App from './components/App';
 import reducer from './reducers';
 import middleWare from './middleware';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer, middleWare);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'))
