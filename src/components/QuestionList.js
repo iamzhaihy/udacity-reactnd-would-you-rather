ca66066d5263750d2d28956f6d866ddc5aa4174f
harvey.zh <hyzhai@ucdavis.edu>
2018-12-26 23:02:52 -0800

    chore: convert QuestionList to a function

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import Question from './Question';
 
 const style = {
@@ -8,25 +7,17 @@ const style = {
     border: '2px solid black'
 };
 
-class QuestionList extends Component {
-    render() {
-        const { questions } = this.props;
-        
-        return (
-            <div style={style}>
-                <h1> QuestionList </h1>
-                {questions && Object.keys(questions).map(qid => {
-                    return <Question key={qid} question={questions[qid]}/>
-                })}
-            </div>
-        )
-    }
+const QuestionList = (props) => {
+    const { questions } = props;
+    
+    return (
+        <div style={style}>
+            <h1> QuestionList </h1>
+            {questions && Object.keys(questions).map(qid => {
+                return <Question key={qid} question={questions[qid]}/>
+            })}
+        </div>
+    )
 }
 
-function mapStateToProps(state) {
-    return {
-        questions: state.questions
-    }
-}
-
-export default connect(mapStateToProps)(QuestionList)
+export default QuestionList
