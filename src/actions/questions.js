2bf7fe4dc032638ceb6baffa8800d5eca62ab4ad
harvey.zh <hyzhai@ucdavis.edu>
2019-01-03 16:25:18 -0500

    feat: add answering questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,27 +1,9 @@
+import { handleReceiveUsers } from "./users";
 import { _getQuestions, _saveQuestionAnswer } from "../utils/_DATA"
 
-export const ADD_QUESTION = "ADD_QUESTION";
-export const ANSWER_QUESTION = "ANSWER_QUESTION";
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
 
 // action creaters
-function add_question(optionOneText, optionTwoText, author){
-    return {
-        type: ADD_QUESTION,
-        optionOneText,
-        optionTwoText,
-        author
-    }
-}
-
-function answer_question(users, questions){
-    return {
-        type: ANSWER_QUESTION,
-        users,
-        questions
-    }
-}
-
 function receive_questions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
@@ -38,3 +20,14 @@ export function handleReceiveQuestions() {
     }
 }
 
+
+export function handleAnswerQuestion(answerObject) {
+    return (dispatch) => {
+        _saveQuestionAnswer(answerObject)
+            .then(() => {
+                dispatch(handleReceiveQuestions())
+                dispatch(handleReceiveUsers())
+            })
+            .catch((e) => console.assert('Error: failed to save poll'))
+    }
+}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleAnswerQuestion } from '../actions/questions';
 
-const style = {
-    margin: '10px',
-    padding: '10px',
-    border: '2px solid black'
-};
+class Question extends Component {
+    onSubmit(answer) {
+        const { authedUser, question, dispatch } = this.props;
+        const qid = question['id'];
 
-const Question = (props) => {
-    const { author, optionOne, optionTwo } = props.question;
+        dispatch(handleAnswerQuestion({
+            authedUser,
+            qid,
+            answer
+        }))
+    }
 
+    render() {
+        const { authedUser, question } = this.props;
+        const { author, optionOne, optionTwo } = question;
     return (
-        <div style={style}>
-            <h1> Question </h1>
-            <h2> Would you rather? </h2>
-            <p> Asked by {author} </p>
-            <p> Option A: {optionOne["text"]} </p>
-            <p> Option B: {optionTwo["text"]} </p>
+            <div>
+                <h1> Would you rather? </h1>
+                <h5> {`posted by ${author}`} </h5>
+                <div>
+                    <p> {`Option 1: ${optionOne['text']}`} </p>
+                    { questionAnswered(authedUser,question) && <p> {`voted by ${optionOne.votes.join(", ")}`} </p>}
+                </div>
+                <div>
+                    <p> {`Option 2: ${optionTwo['text']}`} </p>
+                    { questionAnswered(authedUser,question) && <p> {`voted by ${optionTwo.votes.join(", ")}`} </p>}
+                </div>
+                <button className='button' onClick={() => this.onSubmit('optionOne')}>Choose Option 1</button>
+                <button className='button' onClick={() => this.onSubmit('optionTwo')}>Choose Option 2</button>
         </div>
     )
 }
+}
 
-export default Question;
+function questionAnswered(uid, question) {
+    let votes1 = question['optionOne']['votes'];
+    let votes2 = question['optionTwo']['votes'];
+    return votes1.includes(uid) || votes2.includes(uid);
+}
+
+function mapStateToProps ({ authedUser, questions }, props) {
+    const { qid } = props.match.params;
+    const question = questions[qid] ? questions[qid] : null;
+    return {
+        question,
+        authedUser
+    };
+}
+
+export default connect(mapStateToProps)(Question)
