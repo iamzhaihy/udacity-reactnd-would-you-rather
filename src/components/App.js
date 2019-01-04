748c603b56831c9873f212a22b0c8cfce32978a5
harvey.zh <hyzhai@ucdavis.edu>
2019-01-04 04:35:54 -0500

    feat: add Routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,15 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/authedUser';
 import { handleReceiveUsers } from '../actions/users';
 import { handleReceiveQuestions } from '../actions/questions';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './Login';
+import NavBar from './NavBar';
+import QuestionList from './QuestionList';
+import Question from './Question';
+import LeaderBoard from './LeaderBoard';
+import NotFound from './NotFound';
+import NewQuestion from './NewQuestion';
 
 class App extends Component {
     componentDidMount() {
@@ -11,24 +20,56 @@ class App extends Component {
     }
 
     render() {
+        const { authedUser, dispatch } = this.props;
         return (
-            <div>
+            <BrowserRouter>
+                <div>
+                    <NavBar />
                     {!authedUser
                         ? <Login />
                         : <div>
-                            <div> 
-                                {`Logged in as ${authedUser} `} 
-                                <span onClick={() => dispatch(logout(authedUser))}> Logout </span> 
+                            <div style={{fontSize: '1.5em'}} > 
+                                {`Logged in as ${authedUser} `}
+                                <span style={{float:'right', color: 'red'}} onClick={() => dispatch(logout(authedUser))}> Logout </span> 
                             </div>
+                            <Switch>
+                                <Route 
+                                    exact path='/' 
+                                    render={() => <QuestionList qtype="all" />} 
+                                />
+                                <Route 
+                                    path='/answered' 
+                                    render={() => <QuestionList qtype="answered" />} 
+                                />
+                                <Route 
+                                    path='/unanswered' 
+                                    render={() => <QuestionList qtype="unanswered" />} 
+                                />
+                                <Route 
+                                    path='/leaderboard' 
+                                    component={LeaderBoard}
+                                />
+                                <Route 
+                                    path='/add' 
+                                    component={NewQuestion} 
+                                />
+                                <Route 
+                                    path='/questions/:qid' 
+                                    component={Question} 
+                                />
+                                <Route component={NotFound}/>
+                            </Switch>
                           </div>}
-            </div>
+                </div>
+            </BrowserRouter>
         );
     }
 }
 
 function mapStateToProps(state) {
     return {
-        questions: state.questions
+        questions: state.questions,
+        authedUser: state.authedUser
     }
 }
 
