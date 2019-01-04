89f85e0f4a6a7795ab7c0a916ce4cf5fd393a4e1
harvey.zh <hyzhai@ucdavis.edu>
2019-01-04 02:31:58 -0500

    feat: style QuestionList component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,23 +1,78 @@
 import React from 'react';
-import Question from './Question';
-
-const style = {
-    margin: '10px',
-    padding: '10px',
-    border: '2px solid black'
-};
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { List, Image } from 'semantic-ui-react';
 
 const QuestionList = (props) => {
-    const { questions } = props;
+    const { qlist, qtype, user } = props;
+    const { id, avatarURL } = user; 
+    
+    let questions = [];
+
+    switch (qtype) {
+        case 'unanswered':
+            questions = qlist.filter((q) => {
+                let votes1 = q['optionOne']['votes'];
+                let votes2 = q['optionTwo']['votes'];
+                return !votes1.includes(id) && !votes2.includes(id);
+            });
+            break;
+        case 'answered':
+            questions = qlist.filter((q) => {
+                let votes1 = q['optionOne']['votes'];
+                let votes2 = q['optionTwo']['votes'];
+                return votes1.includes(id) || votes2.includes(id);
+            });
+            break;
+        case 'all':
+            questions = qlist;
+            break;
+        default:
+            questions = qlist;
+    }
+
+    questions.sort((x, y) => y['timestamp'] - x['timestamp']);
     
     return (
-        <div style={style}>
-            <h1> QuestionList </h1>
-            {questions && Object.keys(questions).map(qid => {
-                return <Question key={qid} question={questions[qid]}/>
-            })}
+        <div style={{width: '80%', margin: 'auto'}}>
+            <h1 style={{textAlign: 'center'}}> 
+                {`${capitalize(qtype)} Polls`} 
+            </h1>
+            <List relaxed='very' size='huge'>
+                {questions.map(q => {
+                    const { id, optionOne, optionTwo, timestamp } = q;
+                    const date = new Date(timestamp);
+                    return (
+                        <List.Item key={id}>
+                            <Image avatar src={avatarURL} />
+                            <List.Content>
+                                <List.Header>
+                                    <Link to={`/questions/${id}`} key={id}> 
+                                        {`Would you rather ${optionOne.text} OR ${optionTwo.text}?`}
+                                    </Link>
+                                </List.Header>
+                                <List.Description>{`posted at ${date.toDateString()}`}</List.Description>
+                            </List.Content>
+                        </List.Item>
+                    )
+                })}
+            </List>
         </div>
     )
 }
 
-export default QuestionList
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function mapStateToProps({ questions, users, authedUser }, props) {
+    const { qtype } = props;
+    const qlist = Object.keys(questions).map(qid => questions[qid]);
+    return {
+        qtype,
+        qlist,
+        user: users[authedUser]
+    }
+} 
+
+export default connect(mapStateToProps)(QuestionList);
