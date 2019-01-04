831e1bd144e947f309618f8a32a744fe0f26d377
harvey.zh <hyzhai@ucdavis.edu>
2019-01-04 04:36:03 -0500

    feat: add stylesheet

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,7 @@
 import logger from './logger';
 import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
+import 'semantic-ui-css/semantic.min.css';
 
 export default applyMiddleware(
     thunk,
